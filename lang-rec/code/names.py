db9d48107811ff74e22a66b1cc1f58348c45a84e
Jakub Waszczuk <waszczuk.kuba@gmail.com>
2019-11-06 08:05:01 +0100

    Specify utf8 encoding

--- lang-rec/code/names.py
+++ lang-rec/code/names.py
@@ -6,7 +6,7 @@ from core import DataSet
 def load_data(file_path: str) -> DataSet:
     """Load the dataset from a .csv file."""
     data_set = []
-    with open(file_path, 'r') as file:
+    with open(file_path, 'r', encoding='utf8') as file:
         csv_reader = csv.reader(file, delimiter=',')
         for pair in csv_reader:
             data_set.append(pair)
