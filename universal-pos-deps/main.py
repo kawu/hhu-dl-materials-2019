5bc369d56691ea5fa4635f30ab3778911982490e
Jakub Waszczuk <waszczuk.kuba@gmail.com>
2019-12-09 18:53:18 +0100

    Prepare the code with TODOs to fill

--- universal-pos-deps/main.py
+++ universal-pos-deps/main.py
@@ -1,27 +1,71 @@
-from typing import Sequence, Iterator
+from typing import Sequence, Iterable, Set
 
 import torch.nn as nn
 
+from neural.types import TT
+
 from data import Word, POS, Sent
 import data
+from word_embedding import WordEmbedder, AtomicEmbedder
 
 
 # TODO: Implement this class
 class PosTagger(nn.Module):
     """Simple POS tagger based on LSTM.
 
-    * Each input word (token) is embedded separately as an atomic object
+    * Each input word is embedded separately as an atomic object
     * LSTM is run over the word vector representations
     * The output "hidden" representations are used to predict the POS tags
     * Simple linear layer is used for scoring
     """
 
-    def tag(self, sent: Sequence[Word]) -> Sequence[POS]:
-        """Predict the POS tags in the given sentence."""
+    # TODO: implement this method
+    def __init__(self, word_emb: WordEmbedder, tagset: Set[POS]):
         pass
 
+    # TODO: implement this method
+    def forward(self, sent: Sequence[Word]) -> TT:
+        """Calculate the score vectors for the individual words."""
+        # TODO: Embed all the words and create the embedding matrix
+        embs = None
+        # The first dimension should match the number of words
+        assert embs.shape[0] == len(sent)
+        # TODO: The second dimension should match...?
+        assert embs.shape[1] == -1
+        # TODO: Calculate the matrix with the scores
+        scores = None
+        # The first dimension should match the number of words
+        assert scores.shape[0] == len(sent)
+        # TODO: The second dimension should match...?
+        assert scores.shape[1] == -1
+        # Finally, return the scores
+        return scores
 
-def accuracy(tagger: PosTagger, data_set: Iterator[Sent]) -> float:
+    # TODO: implement this method
+    def tag(self, sent: Sequence[Word]) -> Sequence[POS]:
+        """Predict the POS tags in the given sentence."""
+        # POS tagging is to be carried out based on the resulting scores
+        scores = self.forward(sent)
+        # Create a list for predicted POS tags
+        predictions = []
+        # For each word, we must select the POS tag corresponding to the
+        # index with the highest score
+        for score_vect in scores:
+            # TODO: determine the position with the highest score
+            ix = None
+            # TODO: assert the index is within the range of POS tag indices
+            assert 0 <= ix < None
+            # TODO: determine the corresponding POS tag
+            pos = None
+            # Append the new prediction
+            predictions.append(pos)
+        # We should have as many predicted POS tags as input words
+        assert len(sent) == len(predictions)
+        # Return the predicted POS tags
+        return predictions
+
+
+def accuracy(tagger: PosTagger, data_set: Iterable[Sent]) -> float:
     """Calculate the accuracy of the model on the given dataset.
 
     The accuracy is defined as the percentage of the words in the data_set
@@ -38,6 +82,31 @@ def accuracy(tagger: PosTagger, data_set: Iterator[Sent]) -> float:
                 k += 1.
             n += 1.
     return k / n
+
+
+# TODO: implement this function
+def total_loss(tagger: PosTagger, data_set: Iterable[Sent]) -> TT:
+    """Calculate the total total, cross entropy loss over the given dataset."""
+    # Create two lists for target indices (corresponding to POS tags we
+    # want our mode to predict) and the actually predicted scores.
+    target_ixs = []
+    pred_scores = []
+    # Loop over the dataset in order to determine the target POS tags
+    # and the predictions
+    for sent in data_set:
+        # Unzip the sentence into a (list of words, list of target POS tags)
+        (words, gold_tags) = zip(*sent)
+        # TODO: Determine the target POS tag indices and update `target_ixs`
+        # TODO: Determine the predicted scores and update `pred_scores`
+    # TODO: Convert the target indices and the predictions to tensors
+    pass
+    # Make sure the dimensions match
+    assert target_ixs.shape[0] == pred_scores.shape[0]
+    # TODO: In particular, the second dimension of the predicted scores
+    # should correspond to the size of the tagset, i.e.?
+    assert pred_scores.shape[1] == None
+    # TODO: Calculate the loss and return it
+    pass
 
 
 # Training dataset
@@ -70,8 +139,10 @@ tagset = set(
 # Tagset
 print("Tagset:", tagset)
 
-# TODO: finish the implementation of the model:
-# * Create the word embedding neural module
-# * Calculate the POS tagset
-# * Create the POS tagger (PosTagger)
-# * Train the tagger on the training data
+# Create the word embedding module
+word_emb = AtomicEmbedder(word_set, 50)
+
+# Create the tagger
+tagger = PosTagger(word_emb, tagset)
+
+# TODO: train the model (see `train` in `neural/training`)
